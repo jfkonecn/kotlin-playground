499d85d6d7bd1ced9fec981aed0a6e791d366190
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-01-28 13:54:48 -0500

    a

--- src/main/kotlin/com/playground/year2023/day11/Day11.kt
+++ src/main/kotlin/com/playground/year2023/day11/Day11.kt
@@ -32,40 +32,6 @@ fun sumOfDistancesBetweenGalaxies(
             acc
         })
 
-    // var map =
-    // lines.flatMap { it ->
-    // var sb = StringBuilder()
-    // it.forEachIndexed { index, c ->
-    // if (!columnsWithGalaxies.contains(index)) {
-    // for (i in 1..expandFactor) {
-    // sb.append(c)
-    // }
-    // } else {
-    // sb.append(c)
-    // }
-    // }
-    // var s = sb.toString()
-    // if (it.contains("#")) {
-    // listOf(s)
-    // } else {
-    // var l = mutableListOf<String>()
-    // for (i in 1..expandFactor) {
-    // l.add(s)
-    // }
-    // l
-    // }
-    // }
-
-    // var galaxyCoordinates =
-    // map.foldIndexed(mutableListOf<Pair<Int, Int>>(), { row, acc, s ->
-    // s.forEachIndexed { col, c ->
-    // if (c == '#') {
-    // acc.add(Pair(row, col))
-    // }
-    // }
-    // acc
-    // })
-
     var distanceBetweenGalaxies =
         galaxyCoordinates.dropLast(1).mapIndexed { index, (row, col) ->
             galaxyCoordinates.drop(index + 1).map { (row2, col2) ->
