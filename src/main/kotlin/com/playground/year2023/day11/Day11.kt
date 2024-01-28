966ffec714c619fe52938938bec1ce2fec1df3f6
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-01-28 09:27:55 -0500

    a

--- src/main/kotlin/com/playground/year2023/day11/Day11.kt
+++ src/main/kotlin/com/playground/year2023/day11/Day11.kt
@@ -14,39 +14,61 @@ fun sumOfDistancesBetweenGalaxies(
             acc
         })
 
-    var map =
-        lines.flatMap { it ->
-            var sb = StringBuilder()
-            it.forEachIndexed { index, c ->
-                if (!columnsWithGalaxies.contains(index)) {
-                    for (i in 1..expandFactor) {
-                        sb.append(c)
-                    }
-                } else {
-                    sb.append(c)
-                }
-            }
-            var s = sb.toString()
-            if (it.contains("#")) {
-                listOf(s)
-            } else {
-                var l = mutableListOf<String>()
-                for (i in 1..expandFactor) {
-                    l.add(s)
-                }
-                l
-            }
-        }
-
+    var rowOffset = 0
     var galaxyCoordinates =
-        map.foldIndexed(mutableListOf<Pair<Int, Int>>(), { row, acc, s ->
-            s.forEachIndexed { col, c ->
-                if (c == '#') {
-                    acc.add(Pair(row, col))
+        lines.foldIndexed(mutableListOf<Pair<Int, Int>>(), { row, acc, s ->
+            var columnOffset = 0
+            if (!s.contains("#")) {
+                rowOffset += expandFactor - 1
+            } else {
+                s.forEachIndexed { col, c ->
+                    if (!columnsWithGalaxies.contains(col)) {
+                        columnOffset += expandFactor - 1
+                    } else if (c == '#') {
+                        acc.add(Pair(row + rowOffset, col + columnOffset))
+                    }
                 }
             }
             acc
         })
+
+    galaxyCoordinates.forEach { (row, col) ->
+        println("$row, $col")
+    }
+
+    // var map =
+    // lines.flatMap { it ->
+    // var sb = StringBuilder()
+    // it.forEachIndexed { index, c ->
+    // if (!columnsWithGalaxies.contains(index)) {
+    // for (i in 1..expandFactor) {
+    // sb.append(c)
+    // }
+    // } else {
+    // sb.append(c)
+    // }
+    // }
+    // var s = sb.toString()
+    // if (it.contains("#")) {
+    // listOf(s)
+    // } else {
+    // var l = mutableListOf<String>()
+    // for (i in 1..expandFactor) {
+    // l.add(s)
+    // }
+    // l
+    // }
+    // }
+
+    // var galaxyCoordinates =
+    // map.foldIndexed(mutableListOf<Pair<Int, Int>>(), { row, acc, s ->
+    // s.forEachIndexed { col, c ->
+    // if (c == '#') {
+    // acc.add(Pair(row, col))
+    // }
+    // }
+    // acc
+    // })
 
     var distanceBetweenGalaxies =
         galaxyCoordinates.dropLast(1).mapIndexed { index, (row, col) ->
